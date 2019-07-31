d3a8fc8b0632511e12af88c52a8fe625abe7dcdd
Volosianskyi <o.volosianskyi@sloboda-studio.com>
2019-07-31 13:06:22 +0300

    added model

---
+++ db/migrate/20190731100545_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.primary_key :id
+      t.string :text
+      t.boolean :done
+
+      t.timestamps
+    end
+  end
+end
